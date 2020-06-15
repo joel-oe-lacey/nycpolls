8c03ce22348fcbaa163f23876d4e64eda084ce6e
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-15 13:06:50 -0600

    Updates

---
+++ precache-manifest.a7ab5d60506989b0de57ad0244adc087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984435ee8dc159405c9c589787cb0b50",
+    "url": "/nycpolls/index.html"
+  },
+  {
+    "revision": "114ce2a2040f6e0b545c",
+    "url": "/nycpolls/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "b0cb51a47e74544708ea",
+    "url": "/nycpolls/static/css/main.500bbf23.chunk.css"
+  },
+  {
+    "revision": "114ce2a2040f6e0b545c",
+    "url": "/nycpolls/static/js/2.193ff9fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nycpolls/static/js/2.193ff9fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0cb51a47e74544708ea",
+    "url": "/nycpolls/static/js/main.d095cc7b.chunk.js"
+  },
+  {
+    "revision": "c66e0632715bc8a50adf",
+    "url": "/nycpolls/static/js/runtime-main.7061052c.js"
+  }
+]);
