1aa9554fb788411f076890acd86ab32f272e16d0
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-15 12:36:46 -0600

    Refactor to react-mapbox-gl

---
+++ src/components/MapGL/MapGL.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './MapGL.scss';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+const Map = ReactMapboxGl({
+    accessToken: "pk.eyJ1Ijoiam9lbC1vZS1sYWNleSIsImEiOiJja2Jnb3h5enoxN2tlMndtcmV0Zjl4YmJxIn0.xwAtV-aaryVr5CNqRs9LxQ"
+});
+
+export default class MapGL extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lng: -73.935242,
+            lat: 40.730610,
+            zoom: [10],
+        };
+    }
+
+    render() {
+        const MAPBOX_STYLE = 'mapbox://styles/joel-oe-lacey/ckbgpw4dh0bvv1ipdym0olpg4';
+        const createPollingFeatures = this.props.pollingData.reduce((acc, poll) => {
+                if (poll.location) {
+                    acc.push(
+                        <Feature
+                            coordinates={[poll.location.longitude, poll.location.latitude]}
+                            properties={poll}
+                        />)
+                }
+                return acc;
+            }, [])
+
+        return (
+            <Map 
+                className='map'
+                style={MAPBOX_STYLE}
+                center={[this.state.lng, this.state.lat]}
+                zoom={this.state.zoom}
+            >
+                <Layer type="circle" id="marker"
+                >
+                    {createPollingFeatures}
+                </Layer>
+            </Map>
+        )
+    }
+};
