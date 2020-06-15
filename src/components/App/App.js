1ea0c52c54b824fb753ed18aafb3050496c09877
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-15 11:50:48 -0600

    Move App file to components

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Map from '../Map/Map';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
