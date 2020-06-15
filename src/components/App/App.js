4402456fe5cf349d008048ace7060e14a42bce7a
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-15 12:37:19 -0600

    Abstract SODA fetch call to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
-import Map from '../Map/Map';
+import Map from '../MapGL/MapGL';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
+export default class App extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pollLocations: []
+    };
+  }
+
+  fetchPollingPoints = async () => {
+    try {
+      const settings = {
+        method: "GET", headers: { "Content-Type": "application/json", 'X-App-Token': "I4HU56w9UWCUEicW4AxsHDNck" }
+      };
+      const response = await fetch('https://data.cityofnewyork.us/resource/utqd-4534.json', settings);
+      if (!response.ok) {
+        throw new Error('API error');
+      }
+      const pollData = await response.json();
+      this.setState({ pollLocations: pollData })
+    }
+    catch (e) {
+      console.log(e.message);
+    }
+  }
+
+  componentDidMount() {
+    this.fetchPollingPoints()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Map pollingData={this.state.pollLocations} />
+      </div>
+    );
+  } 
 }
-
-export default App;
